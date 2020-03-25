d0262d7feca819d8d16c4f5213a37b22de9c88e6
ystroppa <yvan.stroppa2@univ-lille.fr>
2020-03-25 17:16:46 +0100

    :Version de base pour la construction de l'application de jeu

---
+++ public/scripts/comportement.js
@@ -0,0 +1,64 @@
+    var nbpieces=10;
+    var nave=navigator.userAgent.toLowerCase(); 
+    if (nave.indexOf("firefox") > -1) nave="Firefox"; else nave="Firefox";
+    var fenetre = document.getElementById('canvas'); 
+    var hh=canvas.getContext('2d');
+    hh.canvas.style.border="3px solid #000";
+    hh.translate(0.5,0.5);
+    var Xs,Ys;
+    var Xd=10,Yd=10;
+    var ct,xmin=Xd+30,xmax=xmin+6*a,ymin=Yd+30,ymax=ymin+6*a;
+    trace();
+
+
+// commandes ***********************
+    function choixRaz(){
+    }  
+    
+    function choixSave(){
+    }
+    
+    function stop(){
+    }
+
+    function depart(bt1) {						
+    }
+        
+//   ******************************************
+
+    function drawLine(xi,yi,xf,yf){
+        hh.beginPath();
+        hh.moveTo(xi,yi);
+        hh.lineTo(xf,yf);
+        hh.stroke();} 
+    
+    function cadre3D(x,y,w,ha){
+        hh.strokeStyle="rgb(130,130,133)";            
+        drawLine(x,y+1,w-1,y+1);	drawLine(x,y,x,ha);
+        drawLine(x+1,y,x+1,ha-1);   drawLine(x,y,w,y);
+        hh.strokeStyle="rgb(230,230,230)";  		
+        drawLine(x,ha,w,ha);	drawLine(w,y+1,w,ha);
+        drawLine(x+1,ha-1,w,ha-1);	drawLine(w-1,y+2,w-1,ha);
+    } 
+
+    function affiche(){
+    }	
+
+
+    function trace(){
+        hh.font="bold 16px Arial";
+        hh.textAlign="center";
+        hh.fillStyle="silver";       
+	hh.fillRect(0,0,440,440);
+        hh.fillStyle="black";
+        hh.fillRect(Xd+20,Yd+20,370,370);
+        hh.fillStyle="silver";
+        hh.fillRect(xmin,ymin,350,350);
+        hh.fillStyle="silver";	
+	hh.fillRect(388,160,12,55);
+        hh.fillStyle="red";	
+	hh.fillText("out",400,190);
+        hh.lineWidth=1; 
+        cadre3D(20,20,420,420);
+    } 
+
